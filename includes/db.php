d38c18e1a70ed3209b0ccc103323b568473ee0b7
Doch-Hsgl-4 <99366238+Doch-Hsgl-4@users.noreply.github.com>
2025-04-13 18:14:56 +0300

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'education_platform';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+?>
